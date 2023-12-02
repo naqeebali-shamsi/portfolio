65e875cc0bfa34773bc792fc19d6971d39766162
Naqeebali Naveed Shamsi <nq957544@dal.ca>
2023-12-02 18:07:27 -0400

    Added custom icons

---
+++ src/components/customIcons/iconData.js
@@ -0,0 +1,6 @@
+import { ReactComponent as TerraformLogo } from '../../assets/images/terraform.svg';
+
+export const iconData = {
+  "Terraform": <TerraformLogo />,
+  // Add more custom icons as needed
+};
